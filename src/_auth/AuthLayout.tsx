3705cfc16eb4d633cbb26a3457e4c9c6a177927e
Raivals <103427644+Raivals@users.noreply.github.com>
2023-11-28 14:59:59 +0100

    Edit Form with shadcn-ui

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,25 @@
-import React from "react"
+import { Outlet, Navigate } from "react-router-dom"
 
 const AuthLayout = () => {
-  return <div>AuthLayout</div>
+  const isAuthenticated = false
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-col items-center justify-center flex-1 py-10">
+            <Outlet />
+          </section>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden object-cover w-1/2 h-screen bg-no-repeat xl:block"
+          />
+        </>
+      )}
+    </>
+  )
 }
 
 export default AuthLayout
