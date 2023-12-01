a2c1a7e1a01c742bfbbed8270b7d8f849657f03a
Raivals <103427644+Raivals@users.noreply.github.com>
2023-12-01 14:15:39 +0100

    Import of react query

--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -1,1 +1,15 @@
-import {} from "@tanstack/react-query"
+import { INewUser } from "@/types"
+// Import react query
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  useInfiniteQuery,
+} from "@tanstack/react-query"
+
+// Initialise a new mutation fonction
+export const useCreateUserAccountMutation = () => {
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  })
+}
