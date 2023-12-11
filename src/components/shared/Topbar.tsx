7c1c0439e6f28be9bb37ede072708d12a782ed71
Raivals <103427644+Raivals@users.noreply.github.com>
2023-12-11 13:06:42 +0100

    Homepage component creation

---
+++ src/components/shared/Topbar.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+import { Button } from "../ui/button"
+
+const Topbar = () => {
+  return (
+    <section className="topbar">
+      <div className="px-5 py-4 flex-between">
+        <Link to="/" className="flex items-center gap-3">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={130}
+            height={325}
+          />
+        </Link>
+        <div className="flex gap-4">
+          <Button>
+            <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Topbar
