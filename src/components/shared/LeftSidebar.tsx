d2c0601181ec4bbed57f1666f1630098e4a3412c
Raivals <103427644+Raivals@users.noreply.github.com>
2023-12-14 12:38:36 +0100

    leftsidebar init

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,44 @@
-import React from "react"
+import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { Button } from "../ui/button"
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutation"
+import { useEffect } from "react"
+import { useUserContext } from "@/context/AuthContext"
 
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>
+  const { mutate: signOut, isSuccess } = useSignOutAccount()
+  const navigate = useNavigate()
+  const { user } = useUserContext()
+
+  useEffect(() => {
+    if (isSuccess) navigate(0)
+  }, [isSuccess])
+
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex items-center gap-3">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={170}
+            height={36}
+          />
+        </Link>
+        <Link to={`/profile/${user.id}`} className="flex items-center gap-3">
+          <img
+            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="rounded-full h-14 w-14"
+          />
+          <div className="flex flex-col ">
+            <p className="body-bold">{user.name}</p>
+            <p className="small-regular text-light-3">@{user.username}</p>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  )
 }
 
 export default LeftSidebar
